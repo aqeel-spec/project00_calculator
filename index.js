1ee00d2d236ceaea20c251fdbd36e9c73f27d252
Aqeel Shahzad <67902032+aqeel-spec@users.noreply.github.com>
2022-10-11 16:32:39 +0500

    changes in files

--- index.js
+++ index.js
@@ -1,68 +1,74 @@
-"use strict";
-exports.__esModule = true;
-var inquirer_1 = require("inquirer");
-console.log('------------  ** Project00-calculator ** ------------------');
-var questions = [
+import inquirer from 'inquirer';
+
+console.log('------------  ** Project00-calculator ** ------------------')
+
+inquirer
+  .prompt([
     {
         type: 'input',
         name: 'name',
+        //default: 'Dear,',
         message: 'Please enter your name',
-        validate: function (answers) {
-            if (answers === '') {
-                return "Please enter valid name you cannot leave it empty ";
+        validate: (answers) => {
+            if ( answers === ''){
+              return `Please enter valid name you cannot leave it empty ` 
             }
-            return true;
+            return true
         }
+        
     },
     {
         type: 'number',
         name: 'userInpt1',
         message: 'Please enter 1st number (any number): ',
-        validate: function (answers) {
-            if (isNaN(answers)) {
-                return 'Please enter valid number';
+        validate: (answers) => {
+            if ( isNaN(answers) ){
+              return 'Please enter valid number'
             }
-            return true;
+            return true
         }
+
     },
     {
         type: 'number',
         name: 'userInpt2',
         message: 'Please enter 2st number : ',
-        validate: function (answers) {
-            if (isNaN(answers)) {
-                return 'Please enter valid number';
+        validate: (answers) => {
+            if ( isNaN(answers) ){
+              return 'Please enter valid number'
             }
-            return true;
+            return true
         }
+
     },
     {
         type: 'list',
         name: 'operation',
-        choices: ['+', '-', '*', '/'],
-        message: 'Pic the action you want to perform '
+        choices: [ '+' , '-' , '*' , '/' ],
+        message: 'Pic the action you want to perform ',
     }
-];
-inquirer_1["default"]
-    .prompt(questions)
-    .then(function (answers) {
-    var name = answers.name;
-    var userInpt1 = answers.userInpt1;
-    var userInpt2 = answers.userInpt2;
-    var opt2 = answers.operation;
-    var optl = function (syml) {
-        if (syml === '+') {
-            return userInpt1 + userInpt2;
+  ])
+  .then((answers) => {
+        const name = answers.name;
+        const userInpt1 = answers.userInpt1;
+        const userInpt2 = answers.userInpt2;
+        const opt2 = answers.operation;
+        const optl = (syml) => {
+            if ( syml === '+' ){
+                return userInpt1 + userInpt2
+            }else if ( syml === '-' ){
+                return userInpt1 - userInpt2
+            }
+            else if ( syml === '*' ){
+                return userInpt1 * userInpt2
+            }
+            else if ( syml === '/' ){
+                return userInpt1 / userInpt2
+            }
         }
-        else if (syml === '-') {
-            return userInpt1 - userInpt2;
-        }
-        else if (syml === '*') {
-            return userInpt1 * userInpt2;
-        }
-        else if (syml === '/') {
-            return userInpt1 / userInpt2;
-        }
-    };
-    console.log("Dear ".concat(name, " Your Output is : ").concat(optl(opt2), " "));
-});
+        console.log( `Dear ${name} Your Output is : ${optl(opt2)} ` )
+       
+    })
+
+
+    
